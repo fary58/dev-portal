063b8b1eb0b54ab19afd8d8a5d2e2412825dafcf
Fary~ <65035293+fary58@users.noreply.github.com>
2024-07-03 05:56:32 +0100

    first commit

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(db, {
+        // useNewUrlParser: true,
+      });
+      console.log(`MongoDB Connected: {conn.connection.host}`);
+    } catch (error) {
+      console.error(error.message);
+      process.exit(1);
+    }
+  };
+
+
+  module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./config/db'); 
+var bodyParser = require('body-parser')
+
+
+
+app.use(express.json({extended: false}));
+
+
+const port = process.env.por || 5000;
+
+
+//connect to database
+// connectDB();
+
+app.get('/', (req, res) => { 
+    res.send('Hello World!')
+  })   
+
+
+  //Routes
+  app.use('/api/users', require ('./routes/api/users'));
+  app.use('/api/auth', require ('./routes/api/auth'));
+  app.use('/api/posts', require ('./routes/api/posts'));
+  app.use('/api/profile', require ('./routes/api/profile'));
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
